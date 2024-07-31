0b8ccc1abf7df417f0fda98dbbb7e043331d3fb2
jackr276 <jack_m_robbins@me.com>
2024-07-31 19:46:53 -0400

    input working semi well, still need to revise adjacency matrix

--- src/Jack_Robbins_jmr226_3.cpp
+++ src/Jack_Robbins_jmr226_3.cpp
@@ -3,13 +3,81 @@
  * CS 610 SU24, Programming Assignment 3
  */
 
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
+#include <sstream>
 #include <string>
 
 
 void print_matrix(short** adjacency_matrix, int num_vertices){
+	printf("* ");
+	//Print out the top row, which are vertex labels
+	for(int i = 0; i < num_vertices; i++){
+		printf("%d ", i + 1);
+	}
+
+	printf("\n");
+
+	//Now we can print out what we actually have in the matrix
+	for(int i = 0; i < num_vertices; i++){
+		//Print out the column labels
+		printf("%d ", i + 1);
+		for(int j = 0; j < num_vertices; j++){
+			printf("%d ", adjacency_matrix[i][j]);	
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+}
+
+
+short** create_adjacency_matrix(int num_vertices){
+	//Once we know that the input is good, we can define our adjacency matrix
+	short** adjacency_matrix = (short**)malloc(sizeof(short*) * num_vertices);
+
+	//Reserve space for each row in the matrix
+	for(int i = 0; i < num_vertices; i++){
+		adjacency_matrix[i] = (short*)calloc(num_vertices, sizeof(short));
+	}
+
+	return adjacency_matrix;
+}
+
+
+void teardown_adjacency_matrix(short** adjacency_matrix, int num_vertices){
+	for(int i = 0; i < num_vertices; i++){
+		free(adjacency_matrix[i]);
+	}
+
+	free(adjacency_matrix);
+}
+
+
+void parse_and_input_line(std::string line, short** adjacency_matrix){
+	if(line == "" || line == "d") return;
+	//Grab a stringstream of the input
+	std::stringstream input(line);
+	
+	char ch;
+	int from_vertex, to_vertex;
+
+	//The first char should be the 'from' vertex 
+	input.get(ch);
+	
+	from_vertex = ch - '0';
+
+	//Skip whitespace
+	input >> std::ws;
+
+	input.get(ch);
+
+	to_vertex = ch - '0';
+
+	//Populate the adjacency_matrix
+	adjacency_matrix[to_vertex - 1][from_vertex - 1] = 1;	
 	
 }
 
@@ -22,17 +90,12 @@ int main(void){
 
 	//Some simple input validation
 	if(num_vertices <= 0){
-		std::cerr << "Input Error: The number of nodes must be positive." << std::endl;
+		std::cerr << "Input Error: The number of nodes must be a positive integer." << std::endl;
 		exit(1);
 	}
 
-	//Once we know that the input is good, we can define our adjacency matrix
-	short** adjacency_matrix = (short**)malloc(sizeof(short*) * num_vertices);
-
-	//Reserve space for each row in the matrix
-	for(int i = 0; i < num_vertices; i++){
-		adjacency_matrix[i] = (short*)malloc(sizeof(short) * num_vertices);
-	}
+	//Initialize a matrix for us
+	short** input_graph = create_adjacency_matrix(num_vertices);
 
 	//We now need to construct the adjacency matrix based on what the user puts in
 	std::cout << "Enter the adjacency list for the graph that you desire to use. When done entering, enter <d>" << std::endl;
@@ -46,9 +109,11 @@ int main(void){
 	//Keep grabbing input for the adjacency list
 	while(input != "d" && input != "D"){
 		std::getline(std::cin, input);
-						
-	}
-	
+		parse_and_input_line(input, input_graph);		
+	} 
+
+	printf("You entered a graph described by the adjacency matrix below\n");
+	print_matrix(input_graph, num_vertices);
 
 	//Let the environment know all went well
 	return 0;
