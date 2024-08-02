8ed203a1dc6180f82b9721c4839b819671662041
jackr276 <jack_m_robbins@me.com>
2024-08-02 14:57:52 -0400

    transitive closure working?

--- src/Jack_Robbins_jmr226_3.cpp
+++ src/Jack_Robbins_jmr226_3.cpp
@@ -5,17 +5,17 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <ostream>
 #include <sstream>
 #include <string>
 
 
-short** transitive_closure(short** adjacency_matrix, int num_vertices){
-
-}
-
-
+/**
+ * Takes in and displays in a nice format an adjacnecy matrix of dimensions
+ * num_vertices x num_vertices
+ */
 void print_matrix(short** adjacency_matrix, int num_vertices){
 	printf("\t* ");
 	//Print out the top row, which are vertex labels
@@ -39,6 +39,10 @@ void print_matrix(short** adjacency_matrix, int num_vertices){
 }
 
 
+/**
+ * Dynamically allocates and returns a reference to an adjacency matrix of dimensions
+ * num_vertices x num_vertices
+ */
 short** create_adjacency_matrix(int num_vertices){
 	//Once we know that the input is good, we can define our adjacency matrix
 	short** adjacency_matrix = (short**)malloc(sizeof(short*) * num_vertices);
@@ -48,6 +52,7 @@ short** create_adjacency_matrix(int num_vertices){
 		adjacency_matrix[i] = (short*)calloc(num_vertices, sizeof(short));
 	}
 
+	//Give the pointer back to the caller
 	return adjacency_matrix;
 }
 
@@ -113,6 +118,33 @@ void parse_and_input_line(std::string line, short** adjacency_matrix, int num_ve
 }
 
 
+short** transitive_closure(short** adjacency_matrix, int num_vertices){
+	short** transitive_closure = create_adjacency_matrix(num_vertices);
+
+	//We'll just do a quick copy of what we were given into what will eventually become our transitive_closure
+	for(int i = 0; i < num_vertices; i++){
+		for(int j = 0; j < num_vertices; j++){
+			transitive_closure[i][j] = adjacency_matrix[i][j];
+		}
+	}
+
+	for(int k = 0; k < num_vertices; k++){
+	
+		for(int i = 0; i < num_vertices; i++){
+			for(int j = 0; j < num_vertices; j++){
+				transitive_closure[i][j] = transitive_closure[i][j] || transitive_closure[i][k] && transitive_closure[k][j];
+			}
+		}
+
+		//Show the user what we have so far
+		printf("G%d:\n", k);
+		print_matrix(transitive_closure, num_vertices);
+	}	
+
+	return transitive_closure;
+}
+
+
 /**
  * The main function takes in user input and parses it into the adjacency matrix as the user inputs values.
  * Any command line arguments will be disregarded entirely
@@ -147,6 +179,10 @@ int main(void){
 	//Show the user what they've entered
 	printf("You entered a graph described by the adjacency matrix below:\n");
 	print_matrix(input_graph, num_vertices);
+
+	printf("Now finding the transitive closure, G+, of the graph that was inputted\n");
+	short** tran_closure = transitive_closure(input_graph, num_vertices);
+
 
 	//Let the environment know all went well
 	return 0;
