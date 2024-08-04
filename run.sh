674c9c3d014e85f0362f14abb6c218d1ad124ec9
jackr276 <jack_m_robbins@me.com>
2024-08-04 19:08:36 -0400

    fix file naming, runner script

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ fi
 #Clean out the compiled directory
 rm -r out/*
 
-g++ -Wall -Wextra ./src/cpp/Jack_Robbins_jmr226_3.cpp -o ./out/Jack_Robbins_jmr226_3
+g++ -Wall -Wextra ./src/cpp/transitive_closure.cpp -o ./out/transitive_closure
 
-./out/Jack_Robbins_jmr226_3
+./out/transitive_closure
