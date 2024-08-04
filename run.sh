4e0300748075fc880dfc44bec0a362158ef51739
jackr276 <jack_m_robbins@me.com>
2024-08-04 19:06:39 -0400

    initialize rust version

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ fi
 #Clean out the compiled directory
 rm -r out/*
 
-g++ -Wall -Wextra ./src/Jack_Robbins_jmr226_3.cpp -o ./out/Jack_Robbins_jmr226_3
+g++ -Wall -Wextra ./src/cpp/Jack_Robbins_jmr226_3.cpp -o ./out/Jack_Robbins_jmr226_3
 
 ./out/Jack_Robbins_jmr226_3
