3519e96b275c6b95d7f22051ee50d467fd3a677d
jackr276 <jack_m_robbins@me.com>
2024-08-05 14:45:29 -0400

    Update main.rs

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,13 +1,15 @@
-use std::io::Write;
-
 /**
  * Author: Jack Robbins
  * This program finds the transitive closure of a graph that is inputted by the user as a
  * reachability list
+ **/
+
+use std::io::Write;
+
+
+/**
+ * Parse and interpret a line of text that represents reachability, entered by a user
  */
-
-
-
 fn parse_and_interpret(input: String){
 
 }
@@ -17,6 +19,11 @@ fn parse_and_interpret(input: String){
 
 
 
+
+/**
+ * The main entry point to our program. Makes appropriate function calls and grabs user
+ * input
+ */
 fn main() {
     //The number of vertices in our graph
     let num_vertices: i8;
@@ -33,7 +40,7 @@ fn main() {
     //Grab what the user wrote
     std::io::stdin().read_line(&mut user_input).expect("String entered improperly");
 
-   println!("{}", user_input);
+    println!("{}", user_input);
 
 
 
