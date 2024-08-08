95ac72385efa6a77b99c353c11d87b51acdd77a5
jackr276 <jack_m_robbins@me.com>
2024-08-08 16:59:23 -0400

    user input loop working

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -10,7 +10,7 @@ use std::io::Write;
 /**
  * Parse and interpret a line of text that represents reachability, entered by a user
  */
-fn parse_and_interpret(input: String, num_vertices: i8){
+fn parse_and_interpret(input: &String, num_vertices: i8){
 
 }
 
@@ -20,7 +20,6 @@ fn parse_and_interpret(input: String, num_vertices: i8){
  * input
  */
 fn main() {
-  
     //We need a place to store our user's input
     let mut user_input = String::new();
 
@@ -34,9 +33,26 @@ fn main() {
     std::io::stdin().read_line(&mut user_input).expect("String entered improperly");
 
     //Convert to an int, the unwrap will cause a panic if it is bad
-    let num_vertices: i8 = user_input.trim().parse().unwrap();
-
+    let num_vertices = match user_input.trim().parse::<i8>() {
+        Ok(n) => n, 
+        Err(_) => { 
+            println!("Bad input"); 
+            1
+        }
+    };
 
     //Now get the user input line by line for the edge graph
-    println!("Enter the edge list for the graph. When done, enter <d>")
+    println!("Enter the edge list for the graph. When done, enter <d>");
+    
+    //So long as the input isn't "d", we'll keep parsing
+    while !user_input.trim().eq(&String::from("d")) {      
+        user_input.clear();
+        std::io::stdin().read_line(&mut user_input).expect("String entered improperly");
+
+        //Parse and interpret the line into our reachability matrix
+        parse_and_interpret(&user_input, num_vertices)
+
+    }
+    
+
 }
